880bab9af9aef39376a0e82bc760ca8d8bb963d9
Anamika <anamikarecsonbhadra@gmail.com>
2021-11-20 23:28:47 +0530

    restaurant details with filters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import data from "./Component/data.json";
+import RestaurantDetails from "./Component/RestaurantDetails";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterRating: 0,
+      paymentMethod: "all",
+      sortMethod: null
+      // show res which are greater than filterRating
+    };
+  }
+  handleRating = (rating) => {
+    this.setState({
+      filterRating: rating
+    });
+  };
+  handlePayment = (payment) => {
+    this.setState({
+      paymentMethod: payment
+    });
+  };
+
+  handleSort = (order) => {
+    this.setState({
+      sortMethod: order
+    });
+  };
+  render() {
+    const { filterRating, paymentMethod, sortMethod } = this.state;
+    return (
+      <div className="App">
+        <h1 className="header">Restaurant - Details</h1>
+        <div className="feature-btns">
+          <h2>Filter Content By -</h2>
+        <div>
+         <span className="btn-bold"> Ratings :</span>
+          {[4, 3, 2, 1, 0].map((rating) => (
+            <button key={rating} onClick={() => this.handleRating(rating)} className="rating-btn">
+              {rating === 0 ? "All" : rating}
+            </button>
+          ))}
+        </div>
+        <div>
+        <span className="btn-bold"> Payment methods : </span> 
+          {["all", "card", "cash"].map((method) => (
+            <button key={method} onClick={() => this.handlePayment(method)} className="payment-btn">
+              {method}
+            </button>
+          ))}
+        </div>
+        <div>
+        <span className="btn-bold">  Cost : </span> 
+          {["asc", "desc"].map((order) => (
+            <button key={order} onClick={() => this.handleSort(order)} className="payment-btn">
+              {order} 
+            </button>
+          ))}
+        </div>
+        </div>
+        {data
+          .filter(({ rating, payment_methods }) => {
+            const { cash, card } = payment_methods;
+            let paymentCondition = true;
+            if (paymentMethod === "cash") {
+              paymentCondition = cash ? true : false;
+            } else if (paymentMethod === "card") {
+              paymentCondition = card ? true : false;
+            }
+            return rating >= filterRating && paymentCondition;
+          })
+          .sort((a, b) => {
+            if (sortMethod === null) {
+              return 0;
+            }
+            if (sortMethod === "asc") {
+              return a.costForTwo - b.costForTwo;
+            }
+            if (sortMethod === "desc") {
+              return b.costForTwo - a.costForTwo;
+            }
+          })
+          .map((item) => (
+            <RestaurantDetails data={item} key={item.id} />
+          ))}
+      </div>
+    );
+  }
 }
-
-export default App;
